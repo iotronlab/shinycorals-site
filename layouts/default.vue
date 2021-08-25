228e7db2be2ea6d660578ee1fe14892c9b96b234
sandeep <sandeepsinghrana100@gmail.com>
2021-08-25 18:40:20 +0530

    first commit

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,117 +1,49 @@
 <template>
-  <v-app dark>
-    <v-navigation-drawer
-      v-model="drawer"
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      fixed
-      app
-    >
-      <v-list>
-        <v-list-item
-          v-for="(item, i) in items"
-          :key="i"
-          :to="item.to"
-          router
-          exact
-        >
-          <v-list-item-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title v-text="item.title" />
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-app-bar
-      :clipped-left="clipped"
-      fixed
-      app
-    >
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-btn
-        icon
-        @click.stop="miniVariant = !miniVariant"
-      >
-        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="clipped = !clipped"
-      >
-        <v-icon>mdi-application</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="fixed = !fixed"
-      >
-        <v-icon>mdi-minus</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title" />
-      <v-spacer />
-      <v-btn
-        icon
-        @click.stop="rightDrawer = !rightDrawer"
-      >
-        <v-icon>mdi-menu</v-icon>
-      </v-btn>
-    </v-app-bar>
-    <v-main>
-      <v-container>
-        <Nuxt />
-      </v-container>
-    </v-main>
-    <v-navigation-drawer
-      v-model="rightDrawer"
-      :right="right"
-      temporary
-      fixed
-    >
-      <v-list>
-        <v-list-item @click.native="right = !right">
-          <v-list-item-action>
-            <v-icon light>
-              mdi-repeat
-            </v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-footer
-      :absolute="!fixed"
-      app
-    >
-      <span>&copy; {{ new Date().getFullYear() }}</span>
-    </v-footer>
-  </v-app>
+  <div>
+    <div id="navbar">
+      <div class="navbar-links">
+      <nuxt-link to="/about">ABOUT US</nuxt-link>
+      <nuxt-link to="/classprogram">CLASSROOM PROGRAMMES</nuxt-link>
+      <nuxt-link to="/gallery">GALLERY</nuxt-link>
+      <nuxt-link to="/contactus">CONTACT US</nuxt-link>
+      <nuxt-link to="/notice">NOTICES</nuxt-link>
+      </div>
+    </div>
+    <div id="default-body">
+      <nuxt />
+    </div>
+    <div id="footer">
+
+   © 2019 All rights reserved.
+    </div>
+  </div>
 </template>
 
-<script>
-export default {
-  data () {
-    return {
-      clipped: false,
-      drawer: false,
-      fixed: false,
-      items: [
-        {
-          icon: 'mdi-apps',
-          title: 'Welcome',
-          to: '/'
-        },
-        {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire'
-        }
-      ],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'Vuetify.js'
-    }
-  }
+
+<style scoped>
+#navbar{
+background-color: black;
+height: 60px;
 }
-</script>
+#navbar .navbar-links{
+  max-width: 800px;
+  margin: auto;
+  padding-top: 20px;
+}
+#navbar a{
+  color: white;
+  text-decoration: none;
+ padding-left: 10px;
+ margin-top: 2px;
+}
+
+#default-body{
+  max-width: 800px;
+  margin: auto;
+}
+#footer{
+  background-color: rgb(211, 26, 112);
+height: 60px;
+}
+
+</style>

c1afe414d11c6395269b03862bab670a6f22b764
iotronlab <iotronlabspvt@gmail.com>
2021-08-29 22:55:24 +0530

    added loader image

--- pages/index.vue
+++ pages/index.vue
@@ -2,13 +2,12 @@
   <v-container fluid class="pa-0">
     <v-row no-gutters>
       <v-sheet class="mt-n16" width="100%" color="transparent">
-         <v-img
-            :lazy-src="require('@/assets/img/loader.webp')"
-            :src="require('@/assets/img/loader.webp')"
-            eager
-            contain
-          ></v-img>
-
+        <v-img
+          :lazy-src="require('@/assets/img/loader.jpg')"
+          :src="require('@/assets/img/loader.jpg')"
+          eager
+          max-height="850"
+        ></v-img>
       </v-sheet>
       <v-container fluid :class="{ content: $vuetify.breakpoint.lgAndUp }">
         <v-row justify="center" align="center">
@@ -23,56 +22,46 @@
       </v-container>
     </v-row>
 
-<section>
-  <about/>
-</section>
+    <section>
+      <about />
+    </section>
 
     <section id="upper-classroom" class="pa-8 text-center">
       <v-row>
-        <v-col
-        cols="12"
-        sm="6"
-        md="8"
-      >
-      <h1 class="text-h5">
-        "Early childhood education is a very important aspect of a child’s life. Human brain is always receptive <span><strong>no matter how it learns</strong></span> ."
-      </h1>
-      <p>Sampa Bhowmick ~ Founder.</p>
+        <v-col cols="12" sm="6" md="8">
+          <h1 class="text-h5">
+            "Early childhood education is a very important aspect of a child’s
+            life. Human brain is always receptive
+            <span><strong>no matter how it learns</strong></span> ."
+          </h1>
+          <p>Sampa Bhowmick ~ Founder.</p>
         </v-col>
-        <v-col
-        cols="6"
-        md="4"
-      >
-      <v-btn
-              color="success"
-              fab
-              x-large
-              dark
-            >Find</v-btn>
-      </v-col>
+        <v-col cols="6" md="4">
+          <v-btn color="success" fab x-large dark>Find</v-btn>
+        </v-col>
       </v-row>
     </section>
 
     <section class="mt-4 deep-purple lighten-5" id="Classroom-program">
       <h1 class="text-h3 text-center ma-4">Classroom Programmes</h1>
-      <p class="text--disabled text-center ma-4">Designed to develope a child's seven areas of learning.</p>
+      <p class="text--disabled text-center ma-4">
+        Designed to develope a child's seven areas of learning.
+      </p>
 
-
-  <section class="deep-orange lighten-4 ">
-       <program-detail/>
-  </section>
-  </section>
-</v-container>
+      <section class="deep-orange lighten-4">
+        <program-detail />
+      </section>
+    </section>
+  </v-container>
 </template>
 
 <script>
-import { mdiCheckDecagram } from '@mdi/js';
-import ProgramDetail from '~/components/ProgramDetail.vue';
-import About from '~/components/About.vue';
-
+import { mdiCheckDecagram } from '@mdi/js'
+import ProgramDetail from '~/components/ProgramDetail.vue'
+import About from '~/components/About.vue'
 
 export default {
-  components: { ProgramDetail,About },
+  components: { ProgramDetail, About },
   data() {
     return {
       icons: {
@@ -104,11 +93,8 @@ export default {
           }, 500)
         }
       })
-
     },
   },
-
-
 }
 </script>
 <style scoped>
